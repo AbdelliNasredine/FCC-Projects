a1880367058fe77ea673e8166dee108ef3d4469f
AbdelliNasredine <abdou.nasredine@gmail.com>
2019-10-01 06:05:20 +0100

    working on side menu

--- responsive-web-desing/personal-portfolio-webpage/script.js
+++ responsive-web-desing/personal-portfolio-webpage/script.js
@@ -1,7 +1,13 @@
 window.addEventListener('DOMContentLoaded', function () {
-    var descriptions = document.querySelectorAll('.project-description');
-    console.log(descriptions);
-    descriptions.forEach(function (desc) {
-        desc.innerText = String(desc.innerText).slice(0, 100) + '...';
+    let navigation = document.getElementById('side_navigation');
+    document.querySelectorAll('.project-description')
+            .forEach(function (desc) {
+                    desc.innerText = String(desc.innerText).slice(0, 100) + '...';
+            });
+    document.getElementById('menu').addEventListener('click', function() {
+        navigation.classList.toggle('hidden'); 
+    })
+    document.getElementById('close').addEventListener('click', function() {
+        navigation.classList.toggle('hidden');
     });
 });
