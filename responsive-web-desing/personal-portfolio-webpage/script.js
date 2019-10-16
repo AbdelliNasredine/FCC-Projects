0a9bdf1a197d3c21fa51f6953dd926fea2d9c384
AbdelliNasredine <abdou.nasredine@gmail.com>
2019-10-16 16:31:46 +0100

    slide menu complited

--- responsive-web-desing/personal-portfolio-webpage/script.js
+++ responsive-web-desing/personal-portfolio-webpage/script.js
@@ -5,9 +5,13 @@ window.addEventListener('DOMContentLoaded', function () {
                     desc.innerText = String(desc.innerText).slice(0, 100) + '...';
             });
     document.getElementById('menu').addEventListener('click', function() {
-        navigation.classList.toggle('hidden'); 
+        navigation.style.width = "50%";
+        document.getElementById("close").style.display = "block";
+        console.log("menu btn clicked !");
     })
     document.getElementById('close').addEventListener('click', function() {
-        navigation.classList.toggle('hidden');
+        navigation.style.width = "0";
+        document.getElementById("close").style.display = "none";
+        console.log("close btn clicked !");
     });
 });
