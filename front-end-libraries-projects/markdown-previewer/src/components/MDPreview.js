684bf2c86b7e69e6d6387f75528ef5ebc6aaf65c
AbdelliNasredine <abdou.nasredine@gmail.com>
2020-06-24 21:56:14 +0100

    [front-end-projects][markdown-previewer] : both MDPreview and MDTextArea styles added

--- front-end-libraries-projects/markdown-previewer/src/components/MDPreview.js
+++ front-end-libraries-projects/markdown-previewer/src/components/MDPreview.js
@@ -1,8 +1,14 @@
 import React, { Component } from "react";
+import "../styles/md_preview.scss";
 
 class MDPreview extends Component {
   render() {
-    return <div>MDPreview</div>;
+    return (
+      <div className="md-preview-panel">
+        <div className="header">Preview</div>
+        <div className="view"></div>
+      </div>
+    );
   }
 }
 
